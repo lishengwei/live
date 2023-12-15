21973123690a24ec1f6c8e32fe2a204f19b78418
shengwei.li <shengwei.li@geekbang.com>
2023-12-15 12:59:23 +0800

    auto update

--- convert2m3u8.php
+++ convert2m3u8.php
@@ -1,7 +1,6 @@
 <?php
 include_once 'config.php';
 $content = file_get_contents(LOCAL_DIR . '/zijian2.txt');
-var_dump($content);
 $array   = explode(PHP_EOL, $content);
 $hander  = fopen(LOCAL_DIR . '/zijian_convert.m3u8', 'w');
 echo '开始转换txt -> m3u8' . PHP_EOL;
