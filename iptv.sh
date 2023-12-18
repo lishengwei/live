4038688662668467a91a33c8e239c5e0ba1f72f0
shengwei.li <shengwei.li@geekbang.com>
2023-12-18 11:58:46 +0800

    auto update

--- iptv.sh
+++ iptv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CRTDIR=$(cd "$(dirname "$0")";pwd)
 
+echo run `date +"%Y-%m-%d %H:%M:%S"` > $CRTDIR/iptv.log
 php $CRTDIR/collect_iptv_to_txt.php
 php $CRTDIR/convert2m3u8.php
 cd $CRTDIR
