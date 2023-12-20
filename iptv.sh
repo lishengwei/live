39afefb962253e18d8889fe2e897d9dd45af6439
shengwei.li <shengwei.li@geekbang.com>
2023-12-20 10:00:18 +0800

    auto update

--- iptv.sh
+++ iptv.sh
@@ -2,9 +2,9 @@
 CRTDIR=$(cd "$(dirname "$0")";pwd)
 
 echo run `date +"%Y-%m-%d %H:%M:%S"` > $CRTDIR/iptv.log
-php $CRTDIR/collect_iptv_to_txt.php
+/usr/local/Cellar/php@7.3/7.3.33_7/bin/php $CRTDIR/collect_iptv_to_txt.php
 echo collect_iptv_to_txt done `date +"%Y-%m-%d %H:%M:%S"` >> $CRTDIR/iptv.log
-php $CRTDIR/convert2m3u8.php
+/usr/local/Cellar/php@7.3/7.3.33_7/bin/php $CRTDIR/convert2m3u8.php
 echo convert2m3u8 done `date +"%Y-%m-%d %H:%M:%S"` >> $CRTDIR/iptv.log
 cd $CRTDIR
 if [ -n "$(git status -s)" ]; then
