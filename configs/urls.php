420b13351d3ff53455606bb45984504461d831c0
shengwei.li <shengwei.li@geekbang.com>
2024-01-23 10:54:45 +0800

    修复错误

--- configs/urls.php
+++ configs/urls.php
@@ -8,35 +8,36 @@ return [
         'url'   => 'https://xhdwc.tk/tvlive.txt',
         'hosts' => [],
     ],
-    [
-        'url'     => 'https://tvbox.cainisi.cf/TV/',
-        'host'    => [],
-        'headers' => ['User-Agent: okhttp/3.12.11']
-    ],
+//    [
+//        'url'     => 'https://tvbox.cainisi.cf/TV/',
+//        'host'    => [],
+//        'headers' => ['User-Agent: okhttp/3.12.11']
+//    ],
     [
         'url'   => 'https://ddns.10086.fund:23350/iptv.m3u',
         'hosts' => [],
     ],
-    [
-        'url'   => 'http://8.210.232.168/xclive.txt',
-        'hosts' => []
-    ],
-    [
-        'url'   => 'https://raw.githubusercontent.com/sumingyd/Telecom-Shandong-IPTV-List/main/Unicom-Shandong-Dongying.m3u',
-        'hosts' => [],
-        'proxy' => [
-            'host' => '127.0.0.1',
-            'port' => '7890'
-        ]
-    ],
-    [
-        'url'   => 'https://www.hmg.pw:9999/tv.txt',
-        'hosts' => [],
-        'proxy' => [],
-    ],
-    [
-        'url'   => 'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
-        'hosts' => [],
-        'proxy' => [],
-    ],
+//    [
+//        'url'   => 'http://8.210.232.168/xclive.txt',
+//        'hosts' => []
+//    ],
+//    [
+//        'url'   => 'https://raw.githubusercontent.com/sumingyd/Telecom-Shandong-IPTV-List/main/Unicom-Shandong-Dongying.m3u',
+//        'hosts' => [],
+//        'proxy' => [
+//            'host' => '127.0.0.1',
+//            'port' => '7890'
+//        ]
+//    ],
+    // han 18+
+//    [
+//        'url'   => 'https://www.hmg.pw:9999/tv.txt',
+//        'hosts' => [],
+//        'proxy' => [],
+//    ],
+//    [
+//        'url'   => 'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
+//        'hosts' => [],
+//        'proxy' => [],
+//    ],
 ];
